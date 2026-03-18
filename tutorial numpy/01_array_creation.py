022cf94d203efe56afb76d500eb98827d86322d5
WilliamPerezBeltran <williampbeltranprogramador@gmail.com>
2026-03-18 14:04:02 -0500

    add files

--- tutorial numpy/01_array_creation.py
+++ tutorial numpy/01_array_creation.py
@@ -1,12 +1,8 @@
 import numpy as np 
 
-a1D = np.array([1,2,3,4,5])
-a2D = np.array([[1,2],[6,4]])
-a3D = np.array([[[1,2],[10,20]],[[1,2],[10,20]]])
-                
-print(a1D)
-print(a2D)
-print(a3D)
 
-
-
+unaD = [[1,2,3]]
+dosD = [[1,2,3],[10,20,30]]
+print(unaD)
+print(type(unaD))
+print(dosD.shape)
